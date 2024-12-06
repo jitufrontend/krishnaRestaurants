5445797f2a6089b795aa60407dad3dcd9137a7e3
jitufrontend <jitendramistry500@gmail.com>
2024-12-06 22:34:43 +0530

    logo update

--- restaurant/src/components/Header.jsx
+++ restaurant/src/components/Header.jsx
@@ -1,10 +1,20 @@
-
+import logo from '../assets/krishna.svg'
 const Header = () => {
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <header className='flex justify-between items-center'>
+      <img src={logo} alt="" className='w-20' />
+      <ul className='flex items-center gap-2'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Our Speciality</a></li>
+        <li><a href="#">Best Dishes</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <button className='bg-emerald-600	 px-8 rounded-2xl items-center flex py-4'>Order Now</button>
+    
+    </header>
     </>
   )
 }
